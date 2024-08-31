e9cba7d1aa73fd3c404a7c0d37a19356500fa333
Choda Subramanyam <149325423+subramanyamchoda@users.noreply.github.com>
2024-08-31 17:51:45 +0530

    commit

--- src/rest/About.jsx
+++ src/rest/About.jsx
@@ -1,40 +1,6 @@
 import React from 'react';
-import about2 from './assrets/images/about2.jpeg';
+
 const About = () => {
-  const about = [
-    "Education is the most powerful weapon which you can use to change the world. The beautiful thing about learning is that no one can take it away from you. Education makes a people easy to lead but difficult to drive: easy to govern, but impossible to enslave.",
-    "Discover a world of learning at your fingertips with our online courses. Whether you're aiming to boost your skills for work, explore new hobbies, or simply expand your knowledge, we've got you covered. Our courses offer the freedom to learn at your own pace, wherever you are.",
-    "Learning experiences are like journeys. A great place to learn through the online. Join us today and start your learning journey. The roots of education are bitter, but the fruit is sweet. Education is not the filling of a pail, but the lighting of a fire.",
-    "Led by experienced instructors, our interactive lessons make learning engaging and enjoyable. From business and technology to arts and humanities, there's a course for everyone. Start your learning journey today and open the door to endless possibilities.",
-  ];
-
-  const images = [
-    { 
-      id: 1,
-      className: "img-fluid rounded w-100 wow zoomIn",
-      img: "about2",
-      alt: "Educational journey illustration"
-    },
-    { 
-      id: 2,
-      className: "img-fluid rounded w-75 wow zoomIn",
-      img: "assrets/images/about1.jpg",
-      alt: "Online course platform"
-    },
-    { 
-      id: 3,
-      className: "img-fluid rounded w-75 wow zoomIn",
-      img: "assrets/images/about4.png",
-      alt: "Interactive learning"
-    },
-    { 
-      id: 4,
-      className: "img-fluid rounded w-100 wow zoomIn",
-      img: "assrets/images/about3.jpeg",
-      alt: "Learning community"
-    }
-  ];
-
   return (
     <div className='mt-2 py-3'>
       <h1 className="text-primary text-center animated-from-top text-bold text-st1">About Us</h1>
@@ -42,26 +8,54 @@ const About = () => {
         <div className="row g-1 align-items-center">
           <div className="col-lg-6 text-center animated-from-right">
             <h1 className="mb-4 text-st">Welcome to Learners Academy</h1>
-            {about.map((data, index) => (
-              <p key={index} className="mb-4">
-                <i className="fa-solid fa-arrow-right fa-beat fa-lg text-primary"></i>
-                <em> {data}</em>
-              </p>
-            ))}
+            <p className="mb-4">
+              <i className="fa-solid fa-arrow-right fa-beat fa-lg text-primary"></i>
+              <em> Education is the most powerful weapon which you can use to change the world. The beautiful thing about learning is that no one can take it away from you. Education makes a people easy to lead but difficult to drive: easy to govern, but impossible to enslave.</em>
+            </p>
+            <p className="mb-4">
+              <i className="fa-solid fa-arrow-right fa-beat fa-lg text-primary"></i>
+              <em> Discover a world of learning at your fingertips with our online courses. Whether you're aiming to boost your skills for work, explore new hobbies, or simply expand your knowledge, we've got you covered. Our courses offer the freedom to learn at your own pace, wherever you are.</em>
+            </p>
+            <p className="mb-4">
+              <i className="fa-solid fa-arrow-right fa-beat fa-lg text-primary"></i>
+              <em> Learning experiences are like journeys. A great place to learn through the online. Join us today and start your learning journey. The roots of education are bitter, but the fruit is sweet. Education is not the filling of a pail, but the lighting of a fire.</em>
+            </p>
+            <p className="mb-4">
+              <i className="fa-solid fa-arrow-right fa-beat fa-lg text-primary"></i>
+              <em> Led by experienced instructors, our interactive lessons make learning engaging and enjoyable. From business and technology to arts and humanities, there's a course for everyone. Start your learning journey today and open the door to endless possibilities.</em>
+            </p>
           </div>
           {/* Images */}
           <div className="col-lg-6 animated-from-left">
             <div className="row g-3">
-              {images.map((img) => (
-                <div key={img.id} className="col-6">
-                  <img
-                    className={img.className}
-                    src={img.img}
-                    alt={img.alt}
-                  
-                  />
-                </div>
-              ))}
+              <div className="col-6">
+                <img
+                  className="img-fluid rounded w-100 wow zoomIn"
+                  src="assrets/images/about2.jpeg"
+                  alt="Educational journey illustration"
+                />
+              </div>
+              <div className="col-6">
+                <img
+                  className="img-fluid rounded w-75 wow zoomIn"
+                  src="assrets/images/about1.jpg"
+                  alt="Online course platform"
+                />
+              </div>
+              <div className="col-6">
+                <img
+                  className="img-fluid rounded w-75 wow zoomIn"
+                  src="assrets/images/about4.png"
+                  alt="Interactive learning"
+                />
+              </div>
+              <div className="col-6">
+                <img
+                  className="img-fluid rounded w-100 wow zoomIn"
+                  src="assrets/images/about3.jpeg"
+                  alt="Learning community"
+                />
+              </div>
             </div>
           </div>
         </div>
